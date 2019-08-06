902f7a4dcaf89e9e4c05b65addacab34662b30bd
Held Grijo <von.grijo@pmm.am.gov.br>
2019-08-06 17:23:15 -0400

    Configurando conexão com o banco

--- omninstack8/backend/src/server.js
+++ omninstack8/backend/src/server.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const routes = require("./routes");
 
 const server = express();
 
+mongoose.connect(
+  // "mongodb+srv://omnistack:omnistack@cluster0-keblv.mongodb.net/omnistack8?retryWrites=true&w=majority",
+  "mongodb://localhost:27017/omnistack8",
+  { useNewUrlParser: true }
+);
 server.use(express.json());
 server.use(routes);
 
