45536289e56ae54df051218d2584fe605fbd8233
Held Grijo <von.grijo@pmm.am.gov.br>
2019-08-09 09:36:20 -0400

    Pesquisa usuário no github e grava no bd

--- omninstack8/backend/src/controllers/DevController.js
+++ omninstack8/backend/src/controllers/DevController.js
@@ -1,22 +1,30 @@
 const axios = require("axios");
+const Dev = require("../models/Dev");
 
 module.exports = {
   async store(req, res) {
+    // captura parametro username do corpo da requisição
     const { username } = req.body;
-    let response = [];
 
-    try {
-      response = await axios.get(
-        `http://localhost:1111/users?login=${username}`,
-        {
-          proxy: false
-        }
-      );
-      console.log(response);
-    } catch (error) {
-      console.log(error);
-    }
+    // acessa a api do github para pesquisar os dados do usuário informado
+    const response = await axios.get(
+      `http://localhost:1111/users?login=${username}`,
+      {
+        proxy: false
+      }
+    );
 
-    return res.json(response.data);
+    // deconstrução para pegar apenas os dados desejados da resposta
+    const { name, bio, avatar_url: avatar } = response.data[0];
+
+    //cria o dev no banco de dados
+    const dev = await Dev.create({
+      name,
+      user: username,
+      bio,
+      avatar
+    });
+
+    return res.json(dev);
   }
 };
