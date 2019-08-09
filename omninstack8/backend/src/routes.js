fda581bf5103b20d37e197ab1f5470673e8f9774
Held Grijo <von.grijo@pmm.am.gov.br>
2019-08-09 16:59:09 -0400

    Listar usuários que ainda não foram avaliados

--- omninstack8/backend/src/routes.js
+++ omninstack8/backend/src/routes.js
@@ -5,9 +5,7 @@ const DislikeController = require("./controllers/DislikeController");
 
 const routes = express.Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: `Hello World` });
-});
+routes.get("/devs", DevController.index);
 routes.post("/devs", DevController.store);
 routes.post("/devs/:devId/likes", LikeController.store);
 routes.post("/devs/:devId/dislikes", DislikeController.store);
