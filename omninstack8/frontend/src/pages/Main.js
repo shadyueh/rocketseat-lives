9dafacb099b7b2bde4b5561a6745fb13f045e8fa
Held Grijo <shadyueh@gmail.com>
2019-08-11 10:10:00 -0400

    Criando a página de listagem dos devs

--- omninstack8/frontend/src/pages/Main.js
+++ omninstack8/frontend/src/pages/Main.js
@@ -1,5 +1,33 @@
 import React from 'react';
+import './Main.css';
+
+import logo from '../assets/logo.svg'
+import dislike from '../assets/dislike.svg'
+import like from '../assets/like.svg'
+
+
 
 export default function Main({ match }){
-    return(<h1>{match.params.id}</h1>);
+    return(
+        <div className="main-container">
+            <img src={logo} alt="TinDev" />
+            <ul>
+                <li>
+                    <img src="https://avatars2.githubusercontent.com/u/2254731?v=4" alt="avatar" />
+                    <footer>
+                        <strong>User Test</strong>
+                        <p>Programador e cantor sertanejo nas horas vagas</p>
+                    </footer>
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt="Dislike"/>
+                        </button>
+                        <button type="button">
+                            <img src={like} alt="Like"/>
+                        </button>
+                    </div>
+                </li>
+            </ul>
+    </div>
+    )
 }
