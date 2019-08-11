f26e52c426fa0418f253918596cd57cddb681c84
Held Grijo <shadyueh@gmail.com>
2019-08-10 21:39:13 -0400

    Gerenciando estados e propriedades de componentes (form)

--- omninstack8/frontend/src/pages/Login.js
+++ omninstack8/frontend/src/pages/Login.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Login.css';
 
 import logo from '../assets/logo.svg'
 
-export default function Login(){
+export default function Login({history}){
+
+    const [username, setUsername] = useState('');
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(username);
+        history.push('/main');
+    }
+
     return (
         <div className="login-container">
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <img src={logo} alt="TinDev" />
-                <input type="text" placeholder="Informe seu usuário no GitHub"/>
+                <input type="text" placeholder="Informe seu usuário no GitHub"
+                value={username}
+                onChange={e => setUsername(e.target.value)}/>
                 <button type="submit">Enviar</button>
             </form>
         </div> 
