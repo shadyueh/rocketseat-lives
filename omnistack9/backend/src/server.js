3793c27dcf7dc7e841ebb6bad9b6a264ef7e94eb
Held Grijo <von.grijo@pmm.am.gov.br>
2019-10-02 11:26:22 -0400

    Route Params

--- omnistack9/backend/src/server.js
+++ omnistack9/backend/src/server.js
@@ -2,9 +2,17 @@ const express = require("express");
 
 const app = express();
 
+// req.query = Acessar query params (para filtros)
+// req.params = Acessar route params (edição, delete)
+
 app.post("/users", (req, res) => {
   // query params
   return res.json({ idade: req.query.idade });
+});
+
+app.put("/users/:id", (req, res) => {
+  // route params
+  return res.json({ id: req.params.id });
 });
 
 app.listen(3333);
