11b5e46199dd933149583f1a0b90f9912fa39d81
Held Grijo <shadyueh@gmail.com>
2019-10-03 07:04:59 -0400

    Tratamento caso o usuário não exista

--- omnistack9/backend/src/controllers/SpotController.js
+++ omnistack9/backend/src/controllers/SpotController.js
@@ -1,3 +1,4 @@
+const User = require('../models/User');
 const Spot = require('../models/Spot');
 
 module.exports = {
@@ -5,6 +6,12 @@ module.exports = {
         const { company, techs, price } = req.body;
         const { filename } = req.file;
         const { user_id } = req.headers;
+
+        const user = await User.findById(user_id);
+
+        if (!user) {
+            return res.status(400).json({ error: 'User does not exist.' });
+        }
 
         const spot = await Spot.create({
             user: user_id,
