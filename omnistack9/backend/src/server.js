4285854f5b0218a23e3abdccb2474f0ec24b73fc
Held Grijo <shadyueh@gmail.com>
2019-10-02 21:34:04 -0400

    Configurando a base de dados

--- omnistack9/backend/src/server.js
+++ omnistack9/backend/src/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const routes = require("./routes");
 
 const app = express();
 
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-keblv.mongodb.net/omnistack9?retryWrites=true&w=majority',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
 app.use(express.json());
 app.use(routes);
 
