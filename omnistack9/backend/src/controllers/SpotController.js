b65b9119518af85e056dfa44733e225f981eae55
Held Grijo <shadyueh@gmail.com>
2019-10-04 19:48:41 -0400

    Listagem de spots por tecnologia

--- omnistack9/backend/src/controllers/SpotController.js
+++ omnistack9/backend/src/controllers/SpotController.js
@@ -2,6 +2,13 @@ const User = require('../models/User');
 const Spot = require('../models/Spot');
 
 module.exports = {
+
+    async index(req, res){
+        const { tech } = req.query;
+        const spots = await Spot.find({ techs: tech });
+        return res.json(spots);
+    },
+
     async store(req,res){
         const { company, techs, price } = req.body;
         const { filename } = req.file;
