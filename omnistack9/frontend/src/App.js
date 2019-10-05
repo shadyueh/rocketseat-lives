f29d703991994659a41e54a91a8f5fb96687ea19
Held Grijo <shadyueh@gmail.com>
2019-10-05 18:32:10 -0400

    Layout da tela de inscrição

--- omnistack9/frontend/src/App.js
+++ omnistack9/frontend/src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import './App.css';
 
+import logo from './assets/logo.svg';
+
 function App() {
   return (
-    <h1>Hello React</h1>
+    <div className="container">
+      <img src={logo} alt="AirCNC"/>
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
+        </p>
+        <form action="">
+          <label htmlFor="email">E-MAIL *</label>
+          <input 
+            type="email" 
+            id="email" 
+            placeholder="Seu melhor e-mail"
+          />
+          <button type="submit" className="btn" >Entrar</button>
+        </form>
+      </div>
+    </div>
   );
 }
 
