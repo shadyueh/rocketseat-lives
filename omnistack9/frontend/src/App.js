49df93d84a2a07885930db79974a66bcd158c6e4
Held Grijo <shadyueh@gmail.com>
2019-10-05 20:33:54 -0400

    Usando State para armazenamento; Problema de CORS

--- omnistack9/frontend/src/App.js
+++ omnistack9/frontend/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from './services/api';
 import './App.css';
 
 import logo from './assets/logo.svg';
 
 function App() {
+
+  const [email, setEmail] = useState('');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const response = await api.post('/sessions', { email });
+    console.log(response);
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="AirCNC"/>
@@ -11,12 +21,14 @@ function App() {
         <p>
           Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
         </p>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-MAIL *</label>
           <input 
             type="email" 
             id="email" 
             placeholder="Seu melhor e-mail"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
           />
           <button type="submit" className="btn" >Entrar</button>
         </form>
