3a5bb1f15c033d4b8c9139d5193c63ab75226a84
Held Grijo <shadyueh@gmail.com>
2019-10-06 07:30:11 -0400

    Redirecionando para outra tela

--- omnistack9/frontend/src/pages/Login/index.js
+++ omnistack9/frontend/src/pages/Login/index.js
@@ -1,16 +1,22 @@
 import React, { useState } from 'react';
 import api from '../../services/api';
 
-export default function Login() {
+export default function Login({history}) {
 
     const [email, setEmail] = useState('');
 
     async function handleSubmit(event) {
       event.preventDefault();
+
+      if (!email) {
+          return;
+      }
+      
       const response = await api.post('/sessions', { email });
       const { _id } = response.data;
   
       localStorage.setItem('user', _id);
+      history.push('/dashboard');
     }
 
     return (
