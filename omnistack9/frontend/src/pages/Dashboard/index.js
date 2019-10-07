925ca2aeda73ed34c6d45a25d5dfa44d7b904cd4
Held Grijo <shadyueh@gmail.com>
2019-10-06 22:44:14 -0400

    Corrigindo nome do arquivo style.css -> styles.css no Dashboard

--- omnistack9/frontend/src/pages/Dashboard/index.js
+++ omnistack9/frontend/src/pages/Dashboard/index.js
@@ -2,7 +2,7 @@ import React, { useEffect , useState } from 'react';
 import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
-import './style.css';
+import './styles.css';
 
 export default function Dashboard() {
 
