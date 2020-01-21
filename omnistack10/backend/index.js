c3fc4053d5fbcee9a48202e622815c86c795804f
Held Grijo <held.grijo@gmail.com>
2020-01-21 16:43:02 -0400

    Including nodemon for server restart

--- omnistack10/backend/index.js
+++ omnistack10/backend/index.js
@@ -3,6 +3,6 @@ const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
-  return response.send("Hello World");
+  return response.send("Hello Omnistack");
 });
 app.listen(3333);
