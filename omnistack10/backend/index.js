b11927e3f091398c134585431981698d29827d8d
Held Grijo <held.grijo@gmail.com>
2020-01-22 15:51:24 -0400

    Returning in JSON format

--- omnistack10/backend/index.js
+++ omnistack10/backend/index.js
@@ -3,6 +3,6 @@ const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
-  return response.json("Hello Omnistack");
+  return response.json({ message: "Hello Omnistack" });
 });
 app.listen(3333);
