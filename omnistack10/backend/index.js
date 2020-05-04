083b24e3a713a68abc47a8567ed4164a00775e3e
Held Grijo <held.grijo@gmail.com>
2020-05-04 09:20:02 -0400

    Comentários

--- omnistack10/backend/index.js
+++ omnistack10/backend/index.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const app = express();
 
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmetros:
+
+// Query params: request.query (filtros, ordenação, paginação)
+
 app.get("/", (request, response) => {
   return response.json({ message: "Hello Omnistack" });
 });
